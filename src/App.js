292c304d7f4f569eb1c87ba6539d4bc45011b5fd
Ethan <ethankenneth.bilanes@benilde.edu.ph>
2022-07-26 14:17:58 +0800

    Added User Auth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Layout from './components/Layout';
+import LinkPage from './components/LinkPage';
+import RequireAuth from './components/RequireAuth';
 
 import { Routes, Route } from 'react-router-dom';
 
+
+const ROLES = {
+  'User': 2001,
+  'Editor': 1984,
+  'Admin': 5150
+}
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+      <Route path="linkpage" element={<LinkPage />} />
         {/*Routes for pages */}
 
         {/*Change the path name of the landing page to "/" to default to that view */}
-        <Route path="/" element={<Home />} />
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
 import { useNavigate, Link } from "react-router-dom";
 import { useContext } from "react";
+import AuthContext from "../context/AuthProvider";
 
 const Home = () => {
+    
+    const { setAuth } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    
+    const logout = async () => {
+        // if used in more components, this should be in context 
+        // axios to /logout endpoint 
+        setAuth({});
+        navigate('/linkpage');
+    }
+
     return (
         <section>
             <h1>Home Page in Progress </h1>
@@ -9,6 +22,9 @@ const Home = () => {
             <br />
             <Link to="/Signup">Go to the Signup page</Link>
             <br />
+            <div className="flexGrow">
+                <button onClick={logout}>Sign Out</button>
+            </div>
         </section>
     )
 
